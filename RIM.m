4af0ac7647021b673c0491994314979889aca46b
syucs64 <1487623012@qq.com>
2025-04-15 20:59:47 +0800

    4-15_2

---
+++ RIM.m
@@ -0,0 +1,23 @@
+function [x_star, f_vals, time] = RIM(A, b, x_init, tol)
+
+x = x_init;
+AT = A';
+A2 = norm(A, 2)^2;
+maxiter = 10000;
+A_pinv = pinv(A);
+f_vals = [norm(A * x - abs(x) - b, 2)^2];
+tstart = tic;
+for i = 1:maxiter
+    alpha = 1;
+    %x = x - alpha / A2 * AT * (A * x - abs(x) - b);
+    %x = x - alpha * A_pinv * (A * x - abs(x) - b);
+    D = diag(sign(x));
+    x = (A - D)\ b;
+    f_vals(end + 1) = sum((A * x - abs(x) - b).^2);
+    if norm(f_vals(end) - f_vals(end - 1), 2) < tol
+        break;
+    end
+end
+time = toc(tstart);
+x_star = x;
+
--- test.m
+++ test.m
@@ -3,45 +3,59 @@ clc; clear; close all;
 clear all;
 
 sizes = [1e3, 1e4, 1e5, 1e6]; % 测试不同规模的向量
-num_trials = 100;             % 每个规模测试次数
-n = 1000;
-pl = @(x) (abs(x) + x)/2;  
-num_trials = 1; % 设置随机试验次数
+num_trials = 1;             % 每个规模测试次数
+n = 10000;
+pl = @(x) (abs(x) + x)/2; 
 
 % 预定义存储结果的变量
 x_norms = zeros(num_trials, 1);
 r_norms = zeros(num_trials, 1);
 grad_norms = zeros(num_trials, 1);
 iters = zeros(num_trials, 1);
+time_newton = zeros(num_trials, 1);
+time_prox = zeros(num_trials, 1);
 x_init = zeros(n, 1);
-tol = 1e-9;
+r_norms_prox = zeros(num_trials, 1);
+tol = 1e-23;
 for k = 1:num_trials
     % 每次试验生成新的随机数据
     x = 10 * (rand(n, 1) - rand(n, 1));
     u = 1 * pl(x);
     A = null(u');
     A = A * A';
-    A = A + 1 * eye(n);
+    A = A + 2 * eye(n);
     b = 5 * rand(n, 1);
     
     % 调用求解函数
     [x_star, r_star, iter, grad_norm, time1] = solve_AVE_GN(A, b);
     [x_star1, f_vals, time2] = solve_ave(A, b, x_init, tol);
+    [x_star2, f_vals2, time3] = RIM(A, b, x_init, tol);
     
     % 记录当前试验结果
     x_norms(k) = norm(x_star, inf);
     r_norms(k) = norm(r_star, 2)^2;
     grad_norms(k) = grad_norm;
     iters(k) = iter;
+    time_prox(k) = time2;
+    time_newton(k) = time1;
+    r_norms_prox(k) = f_vals(end);
 end
 
 % 计算平均值
 avg_x_norm = mean(x_norms);
 avg_r_norm = mean(r_norms);
+avg_r_norm_prox = mean(r_norms_prox);
 avg_grad_norm = mean(grad_norms);
 avg_iter = mean(iters);
-
+avg_time_newton = mean(time_newton);
+avg_time_prox = mean(time_prox);
 % 显示平均结果
+
+disp(['Average time newton: ', num2str(avg_time_newton)]);
+disp(['Average time prox: ', num2str(avg_time_prox)]);
+disp(['Average r_norm newton: ', num2str(avg_r_norm)])
+disp(['Average r_norm prox: ', num2str(avg_r_norm_prox)])
+%{
 disp(['Average x_norm: ', num2str(avg_x_norm)]);
 disp(['Average r_norm: ', num2str(avg_r_norm)]);
 disp(['Average grad_norm: ', num2str(avg_grad_norm)]);
@@ -49,3 +63,4 @@ disp(['Average iterations: ', num2str(avg_iter)]);
 
 fprintf('%.7f\n', f_vals(end));
 fprintf('%.7f\n', r_norms);
+%}
